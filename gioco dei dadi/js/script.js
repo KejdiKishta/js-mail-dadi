301263334ac9def1e415297f354673d0314086e4
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-03-29 18:26:38 +0100

    add: dice roll style

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -11,12 +11,41 @@ buttonGenerate.addEventListener("click", function () {
     const randomNumberCpu = Math.floor(Math.random() * 6) + 1;
     console.log(randomNumberUser, randomNumberCpu);
 
+    if (randomNumberUser === 1) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-one.svg">`
+    } else if (randomNumberUser === 2) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-two.svg">`
+    } else if (randomNumberUser === 3) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-three.svg">`
+    } else if (randomNumberUser === 4) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-four.svg">`
+    } else if (randomNumberUser === 5) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-five.svg">`
+    } else if (randomNumberUser === 6) {
+        document.querySelector(".user-img").innerHTML = `<img src="./dice faces/dice-six-faces-six.svg">`
+    }
+
+    if (randomNumberCpu === 1) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-one.svg">`
+    } else if (randomNumberCpu === 2) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-two.svg">`
+    } else if (randomNumberCpu === 3) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-three.svg">`
+    } else if (randomNumberCpu === 4) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-four.svg">`
+    } else if (randomNumberCpu === 5) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-five.svg">`
+    } else if (randomNumberCpu === 6) {
+        document.querySelector(".cpu-img").innerHTML = `<img src="./dice faces/dice-six-faces-six.svg">`
+    }
+
+
     //? consizioni di vincita perdita e pareggio
     if (randomNumberUser > randomNumberCpu) {
-        console.log("user vince");
+        document.querySelector("#winner").innerHTML= `<p class="red">PIPPO VINCE!</p>`
     } else if (randomNumberUser < randomNumberCpu) {
-        console.log("cpu vince");
+        document.querySelector("#winner").innerHTML= `<p class="blue">COMPUTER VINCE!</p>`
     } else {
-        console.log("pareggio");
+        document.querySelector("#winner").innerHTML= `<p>PAREGGIO</p>`
     }
 })
